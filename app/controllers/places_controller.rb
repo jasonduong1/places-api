d4c03cf05cfade1306aad5fe0a9fa239998e47fd
jason <jduong89@gmail.com>
2022-04-12 21:20:12 -0700

    adds restful actions/routes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,42 @@
 class PlacesController < ApplicationController
+  def index
+    places = Place.all.order(:id)
+    render json: places
+  end
+
+  def create
+    place = Place.new(
+      name: params[:name],
+      address: params[:address]
+    )
+    if place.save
+      render json: place
+    else
+      render json: { errors: place.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    place = Place.find_by(id: params[:id])
+    render json: place
+  end
+
+  def update
+    place = Place.find_by(id: params[:id])
+    place.name = params[:name] || place.name
+    place.address = params[:address] || place.address
+    if place.save
+      render json: place
+    else
+      render json: { errors: place.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    place = Place.find_by(id: params[:id])
+    place.destroy
+    render json: { message: "Place successfully destroyed!" }
+  end
+end
+
 end
